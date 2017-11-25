8a251f37b6feb38b65ee6b04463ed25162fb22bf
Ian Wigley <ian.wigley2011@gmail.com>
2017-11-25 17:26:39 +0000

    Simple C# Form GUI for VBCC Compiler
    Simple C# Form GUI for VBCC Compiler - First Commit

---
+++ 68KCompilerGUI/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Interop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(936, 379);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(960, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(139, 22);
+            this.toolStripMenuItem2.Text = "Save";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "New";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(422, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Compile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "68K x-compiler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ 68KCompilerGUI/Form1.cs
@@ -0,0 +1,266 @@
+﻿/********************************************************************************/
+/*                                                                              */
+/*  Set the "VBCC" environment variable to "C:\amiga-dev\targets\m68k-amigaos"  */
+/*  Add the following to the "Path" environment variable to C:\amiga-dev\bin\"  */
+/*                                                                              */
+/*                  To compile execute: vc -o hello hello.c                     */
+/*                                                                              */
+/********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Interop
+{
+    public partial class Form1 : Form
+    {
+        private string path = string.Empty;
+        private string temp = string.Empty;
+        private string loadedFileName = string.Empty;
+        private vc page;
+
+        private List<string> log = new List<string>();
+        private List<string> file = new List<string>();
+
+        public Form1()
+        {
+            // Get current directories immediate parent
+            var parentDir = Directory.GetParent(Directory.GetCurrentDirectory());
+
+            var configPath = parentDir.Parent.FullName.ToString() + @"\amiga-dev-master\";
+            var vbccPath = parentDir.Parent.FullName.ToString() + @"\amiga-dev-master\targets\m68k-amigaos";
+            var binPath = parentDir.Parent.FullName.ToString() + @"\amiga-dev-master\bin";
+
+            var temp = parentDir.Parent.FullName.ToString() + @"/amiga-dev-master/";
+
+            log.Add("configPath = " + configPath);
+            log.Add("vbccPath = " + vbccPath);
+            log.Add("binPath = " + binPath);
+            log.Add("temp0 = " + temp);
+
+            if (temp.Contains("\\"))
+            {
+                var tump = temp.Replace("\\", "/");
+                this.temp = tump;
+            }
+
+            log.Add("temp1 = " + this.temp);
+
+            //https://stackoverflow.com/questions/14553830/set-environment-variables-for-a-process
+            //For getting
+            string Pathsave = System.Environment.GetEnvironmentVariable("VBCC", EnvironmentVariableTarget.User);
+
+            log.Add("Pathsave = " + Pathsave);
+
+            //Set the “VBCC” environment variable to “C:\amiga-dev\targets\m68k-amigaos”.
+            Environment.SetEnvironmentVariable("VBCC", vbccPath, EnvironmentVariableTarget.User);
+
+            // Get the path environment variables
+            string pathvar = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
+
+            log.Add("pathvar = " + pathvar);
+
+
+            if (!pathvar.Contains(binPath))
+            {
+                Environment.SetEnvironmentVariable("Path", pathvar + ";" + binPath, EnvironmentVariableTarget.User);
+            }
+
+            generateConfigFile(vbccPath, configPath);
+
+            InitializeComponent();
+            textBox1.Multiline = true;
+            button1.Enabled = false;
+            toolStripMenuItem2.Enabled = false;
+        }
+
+        // Open File method
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "WigWam Interop";
+            openFileDialog.InitialDirectory = @"*.*";
+            openFileDialog.Filter = "All files (*.*)|*.*|All files (*.c)|*.c";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Clear();
+                path = openFileDialog.FileName;
+                try
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        // Read the input stream & display the contents
+                        while (!sr.EndOfStream)
+                        {
+                            file.Add(sr.ReadLine());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("The file could not be read: " + ex.Message);
+                }
+                finally
+                {
+                    textBox1.Lines = file.ToArray();
+                    loadedFileName = openFileDialog.SafeFileName;
+                    button1.Enabled = true;
+                    this.Text = "68K x-compiler - " + openFileDialog.SafeFileName;
+                    toolStripMenuItem2.Enabled = true;
+                }
+            }
+        }
+
+        // Save File method
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if(loadedFileName != string.Empty)
+            {
+                save(loadedFileName);
+            }
+            else
+            {
+                saveAs();
+            }
+        }
+
+        // Exit the application
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // New File
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            loadedFileName = string.Empty;
+            this.Text = "68K x-compiler";
+        }
+
+        // Compile button click
+        private void button1_Click(object sender, EventArgs e)
+        {
+            compile();
+            File.WriteAllLines("log.log", log);
+        }
+
+        // Method to save the file 
+        private void save(string fileName)
+        {
+            FileStream ostrm = null;
+            StreamWriter writer = null;
+            TextWriter oldOut = Console.Out;
+            try
+            {
+                ostrm = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open " + fileName + " for writing: " + ex.Message);
+                return;
+            }
+            finally
+            {
+                writer = new StreamWriter(ostrm);
+                Console.SetOut(writer);
+
+                foreach (string line in textBox1.Lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                path = ostrm.Name;
+                Console.SetOut(oldOut);
+                writer.Close();
+                ostrm.Close();
+                loadedFileName = Path.GetFileName(fileName);
+                button1.Enabled = true;
+            }
+        }
+
+        private void saveAs()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save File";
+            saveFileDialog.InitialDirectory = @"*.*";
+            saveFileDialog.Filter = "All files (*.*)|*.*|All files (*.c)|*.c";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = saveFileDialog.FileName;
+                save(saveFileDialog.FileName);
+                 this.Text = "68K x-compiler - " + loadedFileName;
+                toolStripMenuItem2.Enabled = true;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveAs();
+        }
+
+
+
+        // Method to start CMD and pass in the relevent command line args
+        private void compile()
+        {
+            //https://stackoverflow.com/questions/4788863/how-to-send-series-of-commands-to-a-command-window-process
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+
+            Process process = Process.Start(processStartInfo);
+
+            if (process != null)
+            {
+                process.StandardInput.WriteLine("cd " + temp);
+                //process.StandardInput.WriteLine("vc -o hello hello.c");
+                var name = loadedFileName.Replace(".c", "");
+                process.StandardInput.WriteLine("vc -o " + name + " " + loadedFileName);
+                process.StandardInput.WriteLine("y");
+                process.StandardInput.WriteLine("y");
+
+                process.StandardInput.Close(); // line added to stop process from hanging on ReadToEnd()
+
+                string outputString = process.StandardOutput.ReadToEnd();
+                var tp = outputString.Replace("Microsoft Windows [Version 6.1.7601]\r\nCopyright (c) 2009 Microsoft Corporation.  All rights reserved.\r\n\r\n", "");
+            }
+        }
+
+        // Method to generate the config file using the template
+        private void generateConfigFile(string prefix, string configPath)
+        {
+            page = new vc();
+            page.WriteLine(@"-cc=vbccm68k -quiet -hunkdebug %s -o= %s %s -O=%ld -I""" + prefix + @"/include/""");
+            page.WriteLine(@"-ccv=vbccm68k -hunkdebug %s -o= %s %s -O=%ld -I""" + prefix + @"/include/""");
+            page.WriteLine(@"-as=vasmm68k_mot -quiet -Fhunk -phxass -opt-fconst -nowarn=62 %s -o %s");
+            page.WriteLine(@"-asv=vasmm68k_mot -Fhunk -phxass -opt-fconst -nowarn=62 %s -o %s");
+            page.WriteLine(@"-rm=del quiet %s");
+            page.WriteLine(@"-rmv=del %s");
+            page.WriteLine(@"-ld=vlink -bamigahunk -x -Bstatic -Cvbcc -nostdlib """ + prefix + @"/lib/startup.o"" %s %s -L""" + prefix + @"/lib/"" -lvc -o %s");
+            page.WriteLine(@"-l2=vlink -bamigahunk -x -Bstatic -Cvbcc -nostdlib %s %s -L""" + prefix + @"/lib/"" -o %s");
+            page.WriteLine(@"-ldv=vlink -bamigahunk -t -x -Bstatic -Cvbcc -nostdlib """ + prefix + @"/lib/startup.o"" %s %s -L""" + prefix + @"/lib/"" -lvc -o %s");
+            page.WriteLine(@"-l2v=vlink -bamigahunk -t -x -Bstatic -Cvbcc -nostdlib %s %s -L""" + prefix + @"/lib/"" -o %s");
+            page.WriteLine(@"-ldnodb=-s -Rshort");
+            page.WriteLine(@"-ul=-l%s");
+            page.WriteLine(@"-cf=-F%s");
+            page.WriteLine(@"-ml=500");
+
+            string pageContent = page.TransformText();
+            File.WriteAllText(configPath + "vc.cfg", pageContent);
+        }
+
+
+    }
+}
